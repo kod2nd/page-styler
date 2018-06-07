56b47405c2d242c5bfbce696ad61b4b498337691
kod2nd <colinongengkiat@gmail.com>
2018-06-07 11:39:40 +0800

    Background colour

---
+++ script.js
@@ -0,0 +1,23 @@
+// function defineClassNameSmaller(){
+//     document.body.className='smaller'
+// }
+
+// function resetClassName(){
+//     document.body.className=''
+// }
+
+// function defineClassNameBigger(){
+//     document.body.className='bigger'
+// }
+
+function defineClassName(x){
+    document.body.className= x.className;
+}
+
+// function changeBackgroundColor(color){
+//     document.body.className = color;
+// }
+
+// let x = document.querySelector("link").rel;
+
+// console.log(x)
